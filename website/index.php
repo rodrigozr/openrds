6f41554e5b7718f6225d8ad8483499096779d271
rrosauro <rrosauro@rdisoftware.com>
2017-04-20 23:21:07 -0300

    Imported website code

---
+++ website/index.php
@@ -0,0 +1,15 @@
+<?php header("Location: http://openrds.sourceforge.net/openrds.htm"); exit; ?>
+<html>
+	<head>
+		<title>OpenRDS - Open Requisition Distribution System</title>
+	</head>
+	<body>
+		Redirecting... Please wait
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-1508304-1";
+urchinTracker();
+</script>
+	</body>
+</html>
---
+++ website/webupdate/latestversion.php
@@ -0,0 +1,4 @@
+requestor-version: <?php echo $_GET["version"]; ?>
+
+latest-version: 1.1-beta
+latest-jar: OpenRDS_1.1-beta.jar
